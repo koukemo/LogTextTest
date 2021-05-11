98254a054393dea662114c086500af87cd895948
koukemo <java.sparrow22.surface@gmail.com>
2021-05-12 01:56:36 +0900

    first commit

---
+++ Assets/Scripts/TestLog.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * 参考ページ : Hololens2のアプリ内でデバッグログのメッセージとスタックトレースを表示する
+ * URL : https://bluebirdofoz.hatenablog.com/entry/2020/09/01/230303
+**/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+// HoloLensフォルダ指定
+# if WINDOWS_UWP
+using Windows.Storage;
+# endif
+
+public class TestLog : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("!!! Test Debug Log Message !!!");
+        Debug.LogError("!!! Test Error Log Message !!!");
+
+#if WINDOWS_UWP
+        var folderpath = KnownFolders.Objects3D.Path;
+        var filepath = Path.Combine(folderpath, "TestPB.bin");
+        Debug.Log(filepath); 
+        Debug.LogError(filepath); 
+#endif
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# LogTextTest
+HoloLens上で `Debug.Log()` の内容を確認する <br>
+参考 : [Hololens2のアプリ内でデバッグログのメッセージとスタックトレースを表示する](https://bluebirdofoz.hatenablog.com/entry/2020/09/01/230303)
+
+## 環境
+- Unity2019.4.18f1
+- MRTK2.6
+- HoloLens 2
+
+
+## 設定
+表示テキストを変更したい場合は以下のスクリプトを変更する <br>
+`Assets/Scripts/TestLog.cs`
